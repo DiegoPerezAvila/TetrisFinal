ee5a9c5214edbca269cd487560b502be5e6c77ce
sandra milena <pradamorasandramilena@gmail.com>
2020-03-28 19:53:08 -0500

    creacion de login

---
+++ app/src/main/java/com/example/persistencia/Persona.java
@@ -0,0 +1,48 @@
+package com.example.persistencia;
+
+public class Persona {
+    public int id;
+    public String nombre;
+    public String correo;
+    public String clave;
+
+    public Persona() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
